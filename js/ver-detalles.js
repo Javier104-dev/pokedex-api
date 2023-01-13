b1ec9ff8f6fdbe0552fb427ed0890e28755bb1fc
Javier Anibal Villca <javieranibalvillca@gmail.com>
2023-01-12 21:27:31 -0300

    Reorganizado el codigo de obtener poderes de los pokemones

--- js/ver-detalles.js
+++ js/ver-detalles.js
@@ -3,39 +3,6 @@ import { pokemonServices } from "./services.js";
 const contenedorHtml = document.querySelector("[data-detalles]");
 const listado = document.querySelector("[data-listado]");
 
-const crearDiv = (nombre, foto, altura, peso, tipo) =>{
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    const contenido =
-    `
-    <div class="pokemon__descripcion">
-        <span class="pokemon__descripcion__item">Nombre: ${nombre}</span>
-        <span class="pokemon__descripcion__item">Altura: ${altura}</span>
-        <span class="pokemon__descripcion__item">Peso: ${peso}</span>
-        <span class="pokemon__descripcion__item">Tipo: ${tipo}</span>
-        <span class="pokemon__descripcion__item">Habilidades:</span>
-        <ul class="pokemon__descripcion__poder">
-            
-        </ul>
-    </div>
-    <div>
-        <img class="pokemon__imagen" src="${foto}" alt="Foto Pokemon">
-    </div>
-    `;
-
-    div.innerHTML = contenido;
-    const ulDiv = div.querySelector(".pokemon__descripcion__poder")
-    pokemonServices.obtenerDetallesPokemon(nombre).then((respuestaJson)=>{
-        const arrarHabilidades = respuestaJson.abilities;
-            arrarHabilidades.forEach((habilidad)=>{
-                const li = document.createElement("li");
-                li.textContent = habilidad.ability.name;
-                ulDiv.appendChild(li);
-            });
-    });
-    return div;
-};
-
 const detallesPokemones = async () =>{
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
@@ -64,3 +31,41 @@ const detallesPokemones = async () =>{
 };
 
 detallesPokemones();
+
+const crearDiv = (nombre, foto, altura, peso, tipo) =>{
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    const contenido =
+    `
+    <div class="pokemon__descripcion">
+        <span class="pokemon__descripcion__item">Nombre: ${nombre}</span>
+        <span class="pokemon__descripcion__item">Altura: ${altura}</span>
+        <span class="pokemon__descripcion__item">Peso: ${peso}</span>
+        <span class="pokemon__descripcion__item">Tipo: ${tipo}</span>
+        <span class="pokemon__descripcion__item">Habilidades:</span>
+        <ul class="pokemon__descripcion__poder">
+            
+        </ul>
+    </div>
+    <div>
+        <img class="pokemon__imagen" src="${foto}" alt="Foto Pokemon">
+    </div>
+    `;
+
+    div.innerHTML = contenido;
+    obtenerPoderesPokemon(nombre, div);
+    return div;
+};
+
+const obtenerPoderesPokemon = async (nombre, div) => {
+    const ulDiv = div.querySelector(".pokemon__descripcion__poder");
+    try{
+        const pokemonSeleccionado = await pokemonServices.obtenerDetallesPokemon(nombre);
+        const arrayHabilidades = pokemonSeleccionado.abilities;
+            arrayHabilidades.forEach((habilidad)=>{
+                const li = document.createElement("li");
+                li.textContent = habilidad.ability.name;
+                ulDiv.appendChild(li);
+            });
+    }catch(error) {alert("Ocurrio un error")};
+};
